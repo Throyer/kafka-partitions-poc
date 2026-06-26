c016bfdb719d4f2529a1f6e45a2d232ee322cff0
rhhsantos <rhhsantos@rd.com.br>
2026-06-25 23:15:36 -0300

    feat: Enhance PedidosConsumer to log additional details including queue name and order number header during message consumption

--- src/main/java/com/example/poc/rabbitmq/PedidosConsumer.java
+++ src/main/java/com/example/poc/rabbitmq/PedidosConsumer.java
@@ -1,6 +1,8 @@
 package com.example.poc.rabbitmq;
 
 import org.springframework.amqp.rabbit.annotation.RabbitListener;
+import org.springframework.amqp.support.AmqpHeaders;
+import org.springframework.messaging.handler.annotation.Header;
 import org.springframework.stereotype.Component;
 
 import lombok.extern.slf4j.Slf4j;
@@ -9,7 +11,16 @@ import lombok.extern.slf4j.Slf4j;
 @Component
 public class PedidosConsumer {
   @RabbitListener(queues = "#{T(com.example.poc.rabbitmq.RabbitMqConfig).QUEUE_NAMES}")
-  public void consume(Order order) {
-    log.info("Pedido recebido: orderNumber={}, user={}", order.orderNumber(), order.user());
+  public void consume(
+      Order order,
+      @Header(AmqpHeaders.CONSUMER_QUEUE) String queue,
+      @Header(name = RabbitMqConfig.HASH_HEADER) String orderNumberHeader) {
+    log.info(
+        "Pedido recebido: queue={}, header {}={}, orderNumber={}, user={}",
+        queue,
+        RabbitMqConfig.HASH_HEADER,
+        orderNumberHeader,
+        order.orderNumber(),
+        order.user());
   }
 }
