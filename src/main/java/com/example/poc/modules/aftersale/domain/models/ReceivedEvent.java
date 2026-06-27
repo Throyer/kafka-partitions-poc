dcfe4a5cfa16d8be66ca108af9919e8c34e4c481
rhhsantos <rhhsantos@rd.com.br>
2026-06-27 13:19:24 -0300

    feat: Enhance ReceivedEvent model by adding orderNumber and createdAt fields with indexing for improved query performance

--- src/main/java/com/example/poc/modules/aftersale/domain/models/ReceivedEvent.java
+++ src/main/java/com/example/poc/modules/aftersale/domain/models/ReceivedEvent.java
@@ -1,10 +1,12 @@
 package com.example.poc.modules.aftersale.domain.models;
 
+import java.time.LocalDateTime;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.Setter;
 import org.springframework.data.annotation.Id;
+import org.springframework.data.mongodb.core.index.Indexed;
 import org.springframework.data.mongodb.core.mapping.Document;
 
 @Document(collection = "received_events")
@@ -15,4 +17,11 @@ import org.springframework.data.mongodb.core.mapping.Document;
 public class ReceivedEvent {
   @Id
   private String id;
+
+  @Indexed
+  private String orderNumber;
+
+  private Event event;
+
+  private LocalDateTime createdAt;
 }
