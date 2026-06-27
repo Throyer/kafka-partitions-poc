256496e7c35a9916b3407adebab64c1aad1fbdd3
rhhsantos <rhhsantos@rd.com.br>
2026-06-27 13:24:38 -0300

    feat: Add findByOrderNumber method to AfterSaleRepository and update UpdateAfterSaleService to handle AfterSale updates based on order number

--- src/main/java/com/example/poc/modules/aftersale/repositories/AfterSaleRepository.java
+++ src/main/java/com/example/poc/modules/aftersale/repositories/AfterSaleRepository.java
@@ -1,6 +1,9 @@
 package com.example.poc.modules.aftersale.repositories;
 
 import com.example.poc.modules.aftersale.domain.models.AfterSale;
+import java.util.Optional;
 import org.springframework.data.mongodb.repository.MongoRepository;
 
-public interface AfterSaleRepository extends MongoRepository<AfterSale, String> {}
+public interface AfterSaleRepository extends MongoRepository<AfterSale, String> {
+  Optional<AfterSale> findByOrderNumber(String orderNumber);
+}
--- src/main/java/com/example/poc/modules/aftersale/services/UpdateAfterSaleService.java
+++ src/main/java/com/example/poc/modules/aftersale/services/UpdateAfterSaleService.java
@@ -1,7 +1,12 @@
 package com.example.poc.modules.aftersale.services;
 
+import com.example.poc.modules.aftersale.domain.models.AfterSale;
 import com.example.poc.modules.aftersale.domain.models.Event;
+import com.example.poc.modules.aftersale.domain.models.ReceivedEvent;
+import com.example.poc.modules.aftersale.repositories.AfterSaleRepository;
+import com.example.poc.modules.aftersale.repositories.ReceivedEventRepository;
 import com.example.poc.shared.messaging.domain.models.message.Message;
+import java.time.LocalDateTime;
 import lombok.AllArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Service;
@@ -9,16 +14,28 @@ import org.springframework.stereotype.Service;
 @Slf4j
 @Service
 @AllArgsConstructor
-public class UpdateAfterSaleService {  
+public class UpdateAfterSaleService {
+  private final AfterSaleRepository afterSaleRepository;
+  private final ReceivedEventRepository receivedEventRepository;
+
   public void update(Message<Event> message) {
     var queue = message.consumerQueue();
     var event = message.getBody();
-    
+    var orderNumber = event.getOrderNumber();
+
     log.info(
       "event received. queue: {}, orderNumber: {}, code: {}",
       queue,
-      event.getOrderNumber(),
+      orderNumber,
       event.getStatusCode()
+    );
+
+    afterSaleRepository
+      .findByOrderNumber(orderNumber)
+      .orElseGet(() -> afterSaleRepository.save(new AfterSale(null, orderNumber)));
+
+    receivedEventRepository.save(
+      new ReceivedEvent(null, orderNumber, event, LocalDateTime.now())
     );
   }
 }
