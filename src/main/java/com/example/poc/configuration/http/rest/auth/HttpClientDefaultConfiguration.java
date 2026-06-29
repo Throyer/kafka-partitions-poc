6ce1b915f350b7f1e20eb632be5f263f5f13565d
rhhsantos <rhhsantos@rd.com.br>
2026-06-29 20:57:55 -0300

    feat: Update Feign logging configuration to allow dynamic logging levels via application properties

--- src/main/java/com/example/poc/configuration/http/rest/auth/HttpClientDefaultConfiguration.java
+++ src/main/java/com/example/poc/configuration/http/rest/auth/HttpClientDefaultConfiguration.java
@@ -1,13 +1,14 @@
 package com.example.poc.configuration.http.rest.auth;
 
-import feign.Logger;
+import feign.Logger.Level;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
 @Configuration
 public class HttpClientDefaultConfiguration {
   @Bean
-  Logger.Level feignLoggerLevel() {
-    return Logger.Level.BASIC;
+  Level logging(@Value("${feign.logging.level:NONE}") Level level) {
+    return level;
   }
 }
